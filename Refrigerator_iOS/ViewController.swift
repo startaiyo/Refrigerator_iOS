a4225d44cc1ab3550784a06925d9c0d9f1bae971
startaiyo <startaiyo0104@gmail.com>
2023-07-03 21:47:01 +0900

    Initial Commit

---
+++ Refrigerator_iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Refrigerator_iOS
+//
+//  Created by 土井星太朗 on 2023/07/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
