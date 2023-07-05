b9bd3bd08aea09a92feeaeadb1e702a35a5140e1
startaiyo <startaiyo0104@gmail.com>
2023-07-05 23:10:19 +0900

    feat: add plus button and header using storyboard

--- Refrigerator_iOS/ViewController.swift
+++ Refrigerator_iOS/ViewController.swift
@@ -9,9 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var headerImageView: UIImageView!
+    @IBOutlet weak var addIngredientButton: UIButton!
+    var headerImage: UIImage!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        addIngredientButton.setTitleColor(UIColor.white, for:.normal)
+        addIngredientButton.backgroundColor = UIColor.blue
+        addIngredientButton.layer.cornerRadius = addIngredientButton.bounds.height / 2
+        headerImage = UIImage(named:"refrigerator_header")
+        headerImageView.image = headerImage
     }
 
 
